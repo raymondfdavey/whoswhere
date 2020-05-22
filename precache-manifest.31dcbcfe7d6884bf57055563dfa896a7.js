70083ee737b4d64d705f6c77fa3da3860a65c125
raymondfdavey <raymondfdavey@gmail.com>
2020-05-22 17:04:26 +0100

    Updates

---
+++ precache-manifest.31dcbcfe7d6884bf57055563dfa896a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5342df792c2c20a4bdcdeab4f4f38eaf",
+    "url": "/whoswhere/index.html"
+  },
+  {
+    "revision": "aaa005fe54a6c3f69c02",
+    "url": "/whoswhere/static/css/main.9cc2cc8e.chunk.css"
+  },
+  {
+    "revision": "3719153f3197f18ca143",
+    "url": "/whoswhere/static/js/2.04cafb17.chunk.js"
+  },
+  {
+    "revision": "43db48fc33d3f9e7a300daa957708f18",
+    "url": "/whoswhere/static/js/2.04cafb17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa005fe54a6c3f69c02",
+    "url": "/whoswhere/static/js/main.43af27b9.chunk.js"
+  },
+  {
+    "revision": "4ac599113bed810f6c09",
+    "url": "/whoswhere/static/js/runtime-main.0e2f6142.js"
+  }
+]);
